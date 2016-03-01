6d743b3d09185cc4cf792476f44ff72167ab7601
Andrew Crowley <crowley.andrew@gmail.com>
2016-03-01 15:13:18 -0800

    Add initial JS assignment and first peer pairing 7.5

---
+++ week-7/eloquent.js
@@ -0,0 +1,37 @@
+// Eloquent JavaScript
+
+
+/*
+Release 2: Reflect
+
+In the text submission box, answer the following questions:
+
+What are the biggest similarities and differences between JavaScript and Ruby?
+Ruby and JavaScript are both object oriented languages?
+Some things are very similar like if and while loops, as well as break, push, pop and join... and calling functions.  There are styles.  In other cases, JavaScript seems to have alot more rules.  Return statements for example have different laws, exiting a function and sending the value back to the spot where the function was called - used to send back a value from a function?.   In Ruby, there are destructive string methods like map! that will modify the source object. Ruby uses the ! bang symbol to notify us of these properties. In JavaScript, there is no such convention for desctructive methods. 
+
+Was some of your Ruby knowledge solidified by learning another language? If so, which concepts?
+
+How do you feel about diving into JavaScript after reading these chapters?
+*/
+
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
